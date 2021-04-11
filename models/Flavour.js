173302b26eea64e437b55982c4487516d6c72e37
PelleLindroth <pellelindroth@gmail.com>
2021-04-11 22:23:40 +0200

    Refactor Flavour Model with async/await. Refactor catch error responses

--- models/Flavour.js
+++ models/Flavour.js
@@ -3,52 +3,52 @@ const pool = require('../db/connection')
 const createFlavour = ({ name }) => {
   if (!name) throw ('Invalid query. Required: name')
 
-  return new Promise((resolve, reject) => {
-    pool.query(
-      `INSERT INTO Flavours (name)
-      VALUES ($1)
-      RETURNING *`,
-      [name],
-      (err, result) => {
-        err && reject(err)
-        !result.rowCount && resolve({ success: false, message: `Could not add flavour ${name} ` })
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await pool.query(
+        `INSERT INTO Flavours (name)
+        VALUES ($1)
+        RETURNING *`,
+        [name])
 
-        resolve({ success: true, flavour: result.rows[0] })
-      }
-    )
-  })
+      !result.rowCount && reject({ message: `Could not add flavour ${name} ` })
+
+      resolve({ success: true, flavour: result.rows[0] })
+    } catch (err) { reject({ code: err.code, message: err.detail }) }
+  }
+  )
 }
 
 const getFlavours = () => {
-  return new Promise((resolve, reject) => {
-    pool.query(
-      `SELECT * FROM Flavours`,
-      (err, result) => {
-        err && reject(err)
-        !result.rows.length && resolve({ success: false, message: 'No flavours found' })
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await pool.query(
+        `SELECT * FROM Flavours`)
 
-        resolve({ success: true, count: result.rowCount, results: result.rows })
-      }
-    )
-  })
+      !result.rows.length && reject({ message: 'No flavours found' })
+
+      resolve({ success: true, count: result.rowCount, results: result.rows })
+    } catch (err) { reject({ code: err.code, message: err.detail }) }
+  }
+  )
 }
 
 const updateFlavour = (name, id) => {
   if (!name || !id) throw ('Invalid query. Required: name in body, id in params')
 
-  return new Promise((resolve, reject) => {
-    pool.query(`
-    UPDATE Flavours
-    SET Name = $1
-    WHERE flavour_id = ${id}
-    RETURNING *`,
-      [name],
-      (err, result) => {
-        err && reject(err)
-        !result.rows.length && resolve({ success: false, message: `Unable to update flavour with id ${id}` })
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await pool.query(`
+      UPDATE Flavours
+      SET Name = $1
+      WHERE flavour_id = ${id}
+      RETURNING *`,
+        [name])
 
-        resolve({ success: true, flavour: result.rows[0] })
-      })
+      !result.rows.length && reject({ message: `Unable to update flavour with id ${id}` })
+
+      resolve({ success: true, flavour: result.rows[0] })
+    } catch (err) { reject({ code: err.code, message: err.detail }) }
   })
 }
 
