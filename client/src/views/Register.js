163c9e897cef5bcb80c6fa187188524968d152d6
PelleLindroth <pellelindroth@gmail.com>
2021-05-09 11:25:29 +0200

    Wrap login button in Link

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import {registerUser} from '../api'
+import { registerUser } from '../api'
 
 const Register = () => {
   const [name, setName] = useState('')
@@ -17,7 +17,7 @@ const Register = () => {
       setError(true)
     } else {
       setError(false)
-      const response = await registerUser({name, email, password})
+      const response = await registerUser({ name, email, password })
 
       if (response.success) {
         setRegistered(true)
@@ -31,31 +31,34 @@ const Register = () => {
     <div className="register">
       <h1>REGISTER NEW ACCOUNT</h1>
       {!registered ? (
-      <form onSubmit={register}>
-        <label htmlFor="name">Username</label>
-        <input onChange={(e) => {setName(e.target.value)}} value={name} type="text"/>
-        <label htmlFor="email">Email</label>
-        <input onChange={(e) => {setEmail(e.target.value)}} value={email} type="text"/>
-        <label htmlFor="password">Password</label>
-        <input onChange={(e) => {setPassword(e.target.value)}} value={password} type="password"/>
-        {error && <p>You need to submit name, email and password</p>} 
-        <button style={{'borderColor': '#FC5A8D', 'marginTop': '2rem'}}><p>SUBMIT</p></button>
-      </form>
+        <form onSubmit={register}>
+          <label htmlFor="name">Username</label>
+          <input onChange={(e) => { setName(e.target.value) }} value={name} type="text" />
+          <label htmlFor="email">Email</label>
+          <input onChange={(e) => { setEmail(e.target.value) }} value={email} type="text" />
+          <label htmlFor="password">Password</label>
+          <input onChange={(e) => { setPassword(e.target.value) }} value={password} type="password" />
+          {error && <p>You need to submit name, email and password</p>}
+          <button style={{ 'borderColor': '#FC5A8D', 'marginTop': '2rem' }}><p>SUBMIT</p></button>
+        </form>
 
       ) : (
         <div className="success">
           <p>YOUR ACCOUNT WAS SUCCEFULLY CREATED</p>
-          <button style={{'borderColor': '#FC5A8D', 'marginTop': '2rem'}}>LOG IN</button>
+          <Link to="/login">
+            <button style={{ 'borderColor': '#FC5A8D', 'marginTop': '2rem' }}>LOG IN</button>
+          </Link>
+
         </div>
       )
       }
       {!registered && (
         <Link to="/" className="back-button-container">
-        <div className="back-button"><p>&lt;&lt; BACK</p></div>
+          <div className="back-button"><p>&lt;&lt; BACK</p></div>
         </Link>
       )}
     </div>
-    )
+  )
 }
 
 export default Register
